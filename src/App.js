1372a793e0a4fc9c85cca2537b19fc49b2920dad
An2ans <antoniojesus_92@hotmail.com>
2021-11-05 16:08:36 +0000

    added firebase db functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./components/homepage/HomePage.jsx";
 import ShopPage from "./components/shop/ShopPage.jsx";
 import SignInPage from "./components/sign-in-page/sign-in-page.jsx";
 import Header from "./components/header/header.jsx";
-import {auth} from "./firebase/firebase.js";
+import {auth, createUserProfileDocument} from "./firebase/firebase.js";
 
 
 class App  extends React.Component {
@@ -20,8 +20,22 @@ constructor(){
 unsubscribeFromAuth = null;
 
 componentDidMount(){
-  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-    this.setState({ currentUser: user})
+  this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    if (userAuth) {
+      const userRef = await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser: {
+            id:snapShot.id,
+            ...snapShot.data()
+          }
+        });
+      });
+
+    }else{
+    this.setState({currentUser: userAuth});
+  }
   });
 }
 
