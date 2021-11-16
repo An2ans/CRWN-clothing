097886de57854bf49198b36cf04a8f06703fba71
An2ans <antoniojesus_92@hotmail.com>
2021-11-16 20:19:22 +0000

    removing all changes as React Router Dom is still not working, trying deployment with heroku

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -13,20 +13,20 @@ import {selectCurrentUser} from "../../redux/user/user.selectors.js";
 
 const Header = ({currentUser, hidden}) => (
   <div className="header">
-    <Link className="logo-container" to="/CRWN-clothing/">
+    <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
     <div className="options">
-      <Link className="option" to="/CRWN-clothing/shop">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/CRWN-clothing/shop">
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
       {currentUser ? (
         <div className="option" onClick={() => auth.signOut()}>SIGN OUT </div>
       ):(
-        <Link className="option" to="/CRWN-clothing/signin">
+        <Link className="option" to="/signin">
           SIGN IN
         </Link>
 
